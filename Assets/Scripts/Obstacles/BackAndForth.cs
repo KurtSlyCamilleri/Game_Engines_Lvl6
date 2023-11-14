9eb86e9130fa4736460919795b7bfedd130d7fe1
EmanuelGauci <103245945+EmanuelGauci@users.noreply.github.com>
2023-11-14 08:48:21 +0100

    merge test
    - merged the code with the level design
    - added some folders for future reference
    - got a new lead on the platform issue(still not working though)
    - added a test obstacle course

--- Assets/Scripts/Obstacles/BackAndForth.cs
+++ Assets/Scripts/Obstacles/BackAndForth.cs
@@ -3,38 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackAndForth : MonoBehaviour
-{
-    
-    
+public class BackAndForth : MonoBehaviour {
     public Collider BackCollider;
     public Collider FrontCollider;
     private Collider colliderOpt;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 10f;
 
-    private bool isMovingForward = true; // Initial direction
+    private bool isMovingForward = true;
+    private Rigidbody rb;
 
     private void Awake() {
         colliderOpt = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
-
-    void Update() {
-        if (IsCollidingWith(BackCollider)) {// Check if the object is colliding with the left collider
-            isMovingForward = true; // Collided with left, so move right
+    private void Update() {
+        if (IsCollidingWith(BackCollider)) {
+            isMovingForward = true;
         }
-        if (IsCollidingWith(FrontCollider)) {// Check if the object is colliding with the right collider
-            isMovingForward = false; // Collided with right, so move left
+        if (IsCollidingWith(FrontCollider)) {
+            isMovingForward = false;
         }
-        if (isMovingForward) {// Move the object based on the current direction
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        } else {
-            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
-        }
+        Vector3 movement = isMovingForward ? Vector3.forward : Vector3.back;
+        transform.position += movement * moveSpeed * Time.deltaTime;
     }
-    
-    private bool IsCollidingWith(Collider collider) {// Function to check if the object is colliding with a specific collider
+    private bool IsCollidingWith(Collider collider) {
         return collider.bounds.Intersects(colliderOpt.bounds);
     }
-    
-
 }
